23c26deab8fa3ff9084be2785a87ec5fb2723f19
Akshay Shah <akshay@akshayshah.org>
2022-03-22 13:59:24 -0700

    Add gRPC health implementation
    Add an implementation of gRPC's health checking protobuf service, along
    with tests.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/bufbuild/connect-grpchealth-go
 
 go 1.18
+
+require (
+	github.com/bufbuild/connect v0.0.0-20220318230559-bc1698b81efc // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ grpchealth_test.go
@@ -0,0 +1,122 @@
+// Copyright 2022 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package grpchealth
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/bufbuild/connect"
+	healthv1 "github.com/bufbuild/connect-grpchealth-go/internal/gen/go/connectext/grpc/health/v1"
+)
+
+func TestHealth(t *testing.T) {
+	const (
+		userFQN = "acme.user.v1.UserService"
+		unknown = "foobar"
+	)
+
+	mux := http.NewServeMux()
+	checker := NewStaticChecker(userFQN)
+	mux.Handle(NewHandler(checker))
+	server := httptest.NewUnstartedServer(mux)
+	server.EnableHTTP2 = true
+	server.StartTLS()
+	defer server.Close()
+
+	client, err := connect.NewClient[healthv1.HealthCheckRequest, healthv1.HealthCheckResponse](
+		server.Client(),
+		server.URL+"/grpc.health.v1.Health/Check",
+		connect.WithGRPC(),
+	)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	t.Run("process", func(t *testing.T) {
+		res, err := client.CallUnary(
+			context.Background(),
+			connect.NewRequest(&healthv1.HealthCheckRequest{}),
+		)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		if Status(res.Msg.Status) != StatusServing {
+			t.Fatalf("got status %v, expected %v", res.Msg.Status, StatusServing)
+		}
+	})
+	t.Run("known", func(t *testing.T) {
+		res, err := client.CallUnary(
+			context.Background(),
+			connect.NewRequest(&healthv1.HealthCheckRequest{Service: userFQN}),
+		)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		if Status(res.Msg.Status) != StatusServing {
+			t.Fatalf("got status %v, expected %v", res.Msg.Status, StatusServing)
+		}
+	})
+	t.Run("unknown", func(t *testing.T) {
+		_, err := client.CallUnary(
+			context.Background(),
+			connect.NewRequest(&healthv1.HealthCheckRequest{Service: unknown}),
+		)
+		if err == nil {
+			t.Fatalf("expected error checking unknown service")
+		}
+		var connectErr *connect.Error
+		if ok := errors.As(err, &connectErr); !ok {
+			t.Fatalf("got %v (%T), expected a *connect.Error", err, err)
+		}
+		if code := connectErr.Code(); code != connect.CodeNotFound {
+			t.Fatalf("got code %v, expected CodeNotFound", code)
+		}
+	})
+	t.Run("watch", func(t *testing.T) {
+		client, err := connect.NewClient[healthv1.HealthCheckRequest, healthv1.HealthCheckResponse](
+			server.Client(),
+			server.URL+"/grpc.health.v1.Health/Watch",
+			connect.WithGRPC(),
+		)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		stream, err := client.CallServerStream(
+			context.Background(),
+			connect.NewRequest(&healthv1.HealthCheckRequest{Service: userFQN}),
+		)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		defer stream.Close()
+		if ok := stream.Receive(); ok {
+			t.Fatalf("got message from Watch")
+		}
+		if stream.Err() == nil {
+			t.Fatalf("expected error from stream")
+		}
+		var connectErr *connect.Error
+		if ok := errors.As(stream.Err(), &connectErr); !ok {
+			t.Fatalf("got %v (%T), expected a *connect.Error", err, err)
+		}
+		if code := connectErr.Code(); code != connect.CodeUnimplemented {
+			t.Fatalf("got code %v, expected CodeUnimplemented", code)
+		}
+	})
+}
